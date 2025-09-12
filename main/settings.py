98fb23072e0455358973b4b44acf6ddb25f6ab99
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-09-12 15:49:39 +0100

    add: heroku url hardcode

--- main/settings.py
+++ main/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY",)
+SECRET_KEY = os.environ.get("SECRET_KEY", "nice_try")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com",]
 CSRF_TRUSTED_ORIGINS = []
 host = os.environ.get("HOST")
 if host:
