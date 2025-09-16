dafda4c906d200e8cb067497d6856f077b5a98f2
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-09-16 16:05:10 +0100

    add: about page

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,11 @@ class Comment(models.Model):
         ordering = ["created_on"]
     def __str__(self):
             return f"{self.title} | written by {self.author}"
+
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
