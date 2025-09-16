22d164cc45758f604357d020e755ec1b283c7cb7
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-09-16 16:37:00 +0100

    fixed about implementations

--- blog/models.py
+++ blog/models.py
@@ -31,10 +31,3 @@ class Comment(models.Model):
     def __str__(self):
             return f"{self.title} | written by {self.author}"
 
-class About(models.Model):
-    title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
