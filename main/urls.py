7621648dc47fb99fc69ff2dc463aed1a54c87e8e
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2025-09-18 13:34:08 +0100

    add: cloudinary image about page

--- main/urls.py
+++ main/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,7 +25,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name='blog-urls'),
 ]
-
-# Serve static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
